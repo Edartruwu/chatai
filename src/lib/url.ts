282734347b4e3af2be21aa62339a519e0f5cadf8
Edartruwu <edartru.contactos@gmail.com>
2024-11-19 18:11:22 -0500

    add backend url sample

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = "http://localhost:3000";
+const back_url = process.env.BACK_URL;
+
+if (back_url === undefined || back_url === null) {
+  throw new Error("No backend url found");
+}
+
+export const BASE_URL = back_url;
