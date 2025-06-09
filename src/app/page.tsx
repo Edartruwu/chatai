8711621b6c93e35825b2ef7d9d9497bc8d9b3ca4
Edartruwu <edartru.contactos@gmail.com>
2025-06-08 20:43:58 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { getUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const user = await getUser();
-  if (user) {
-    if (user.role === "admin") {
-      redirect("/admin");
-    }
-    if (user.role === "linko-user") {
-      redirect("/user");
-    }
-  }
+  redirect("/auth");
 }
